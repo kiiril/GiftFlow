81e536f2e51ce0a8d3b867f6cdbc63201d8ba33e
kiiril <89221217@student.upr.si>
2024-08-03 23:35:04 +0200

    Design post card for scroll feed (not responsive)

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Checkbox, Rating} from "@mui/material";
+import {Favorite, FavoriteBorder} from "@mui/icons-material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faStar} from "@fortawesome/free-regular-svg-icons/faStar";
-import {faHeart as emptyHeart} from "@fortawesome/free-regular-svg-icons/faHeart";
-import {faHeart as fullHeart} from "@fortawesome/free-solid-svg-icons/faHeart"
+import TopicTag from "./TopicTag";
+import {faCoins, faLocationDot, faEye} from "@fortawesome/free-solid-svg-icons";
+
 
 const PostCard = () => {
+
+    const [savePost, setSavePost] = React.useState(false);
+
     return (
-        <div className="card flex-row flex-wrap m-0 p-0" style={{maxWidth: "700px", height: "350px"}}>
+        <div className="card flex-row flex-wrap m-0 p-0" style={{height: "400px"}}>
             <div className="card-header border-0 col-7 h-100">
                 <img
                     src="/images/img.png"
@@ -14,27 +19,39 @@ const PostCard = () => {
                     className="img-fluid h-100 w-100"
                 />
             </div>
-            <div className="card-body px-3 py-3 col-5 h-100">
-                <p>Topic: something</p>
-                <p>
-                    Rating:
-                    <FontAwesomeIcon icon={faStar} className="text-warning"/>
-                    <FontAwesomeIcon icon={faStar} className="text-warning"/>
-                    <FontAwesomeIcon icon={faStar} className="text-warning"/>
-                    <FontAwesomeIcon icon={faStar} className="text-warning"/>
-                    <FontAwesomeIcon icon={faStar} className="text-warning"/>
-                    (4.2)
-                </p>
-                <p>Number of views: 47</p>
-                <p>Location: Krakow</p>
-                <p>Price: 20-40$</p>
-                <div className="d-flex justify-content-between">
-                    <div>
-                        <FontAwesomeIcon icon={emptyHeart} color="red" size="lg"/>
+            <div className="card-body p-2 col-5 h-100">
+                <div className="d-flex flex-row-reverse align-items-between flex-wrap mb-4">
+                    <TopicTag color={"lightblue"}>For him</TopicTag>
+                    <TopicTag color={"lightblue"}>For him</TopicTag>
+                    <TopicTag color={"lightblue"}>For him</TopicTag>
+                </div>
+                <h5 className="text-center mb-3">Title</h5>
+                <p>Description: there is some short description limited by words. Pipi poo poo papa</p>
+                <div className="d-flex align-items-center mb-2">
+                    <Rating value={5}/>
+                    (5.0)
+                </div>
+                <div className="mb-2">
+                    <FontAwesomeIcon icon={faLocationDot} className="ms-1 me-1"/>
+                    Poland, Warsaw
+                </div>
+                <div className="mb-5">
+                    <FontAwesomeIcon icon={faCoins} className="ms-1 me-1"/>
+                    100-200$
+                </div>
+                <div className="d-flex align-items-center">
+                    <div className="me-5">
+                        <Checkbox
+                            icon={<FavoriteBorder style={{color: "red"}}/>}
+                            checkedIcon={<Favorite style={{color: "red"}}/>}
+                            size="large"
+                            onChange={() => setSavePost(!savePost)}
+                        />
                     </div>
-                    <a href="#" className="btn btn-sm btn-outline-secondary">
-                        Details
-                    </a>
+                    <div>
+                        <FontAwesomeIcon icon={faEye} className="me-1"/>
+                        200
+                    </div>
                 </div>
             </div>
         </div>
