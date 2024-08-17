dd916a6bcbcd05fba3e5b4044ce6e3470655297c
kiiril <89221217@student.upr.si>
2024-08-17 12:52:47 +0300

    Implement simple scroll feed logic with observer

--- frontend/src/components/ScrollFeed.jsx
+++ frontend/src/components/ScrollFeed.jsx
@@ -5,16 +5,37 @@ import axios from "axios";
 
 const ScrollFeed = () => {
     const [posts, setPosts] = React.useState([]);
+    const [page, setPage] = React.useState(1);
+    const [limit, setLimit] = React.useState(1);
+    const lastElement = React.useRef(); // last element in the feed
+    const observer = React.useRef(); // observer for infinite scrolling
+
+    useEffect(() => {
+        if (observer.current) observer.current.disconnect();
+        const callback = (entries, observer) => {
+            if (entries[0].isIntersecting) {
+                setPage(page + 1);
+            }
+        }
+        observer.current = new IntersectionObserver(callback);
+        observer.current.observe(lastElement.current);
+    }, [posts]);
 
     const fetchPosts = async () => {
-        const response = await axios.get("http://localhost:8080/posts");
+        const response = await axios.get("http://localhost:8080/posts", {
+            params: {
+                limit: limit,
+                page: page
+            }
+        });
         console.log(response.data)
-        setPosts(response.data);
+        setPosts([...posts, ...response.data]);
+        // setTotalCount(response.headers["x-total-count"]);
     }
 
-    useEffect( ()=>{
+    useEffect( ()=> {
         fetchPosts();
-    }, []);
+    }, [page]);
 
 
     return (
@@ -23,6 +44,8 @@ const ScrollFeed = () => {
             {posts.map(post =>
                 <PostCard key={post.id} post={post}/>
             )}
+            <div ref={lastElement} style={{height: "20px", backgroundColor: "red"}}/>
+            {/* just for indication of the last post, will be removed */}
         </div>
     );
 };
