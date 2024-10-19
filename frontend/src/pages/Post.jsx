03ef5633d1a4639115990b47ef873140f6a497c5
kiiril <89221217@student.upr.si>
2024-10-19 19:12:36 +0200

    Design post page without logic

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import TopicTag from "../components/TopicTag";
 import {Checkbox, Rating} from "@mui/material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCoins, faHeart as fullHeart, faLocationDot} from "@fortawesome/free-solid-svg-icons";
-import {faComment, faHeart as emptyHeart, faPaperPlane} from "@fortawesome/free-regular-svg-icons";
+import {faCoins, faHeart as fullHeart, faLocationDot, faStar as fullStar} from "@fortawesome/free-solid-svg-icons";
+import {faComment, faHeart as emptyHeart, faPaperPlane, faStar as emptyStar} from "@fortawesome/free-regular-svg-icons";
 
 const Post = () => {
     const [postData, setPostData] = React.useState({
@@ -36,6 +36,8 @@ const Post = () => {
         fetchPost();
     }, []);
 
+    const [commentValue, setCommentValue] = useState("")
+
     const savePost = (e) => {
         const {checked} = e.target;
         setPostData(
@@ -48,87 +50,159 @@ const Post = () => {
     }
 
     return (
-        <div className="card flex-wrap m-0 p-0" style={{height: "auto"}}>
-            <div className="row g-0">
-                <div className="col-12 col-md-6">
+        <div className="card border-0" style={{margin: "8rem"}}>
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-6">
                     <div className="carousel slide" id={postData.id}>
+                        <div className="carousel-indicators">
+                            {postData.image_urls.map((_, index) => (
+                                <button
+                                    type="button"
+                                    data-bs-target={`#${postData.id}`}
+                                    data-bs-slide-to={index}
+                                    className={index === 0 ? "active" : ""}
+                                    aria-label={`Slide ${index + 1}`}
+                                    key={index}
+                                    style={{borderRadius: "100%", width: "1rem", height: "1rem"}}
+                                ></button>
+                            ))}
+                        </div>
+
                         <div className="carousel-inner">
                             {postData.image_urls.map((url, index) =>
                                 <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
                                     <img
                                         src={url}
                                         alt="post"
-                                        className="img-fluid h-100 w-100"
-                                        style={{objectFit: "contain", aspectRatio: "1/1"}}
+                                        className="img-fluid"
                                     />
                                 </div>
                             )}
                         </div>
                         <button className="carousel-control-prev" type="button" data-bs-target={`#${postData.id}`}
                                 data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span className="visually-hidden">Previous</span>
                         </button>
                         <button className="carousel-control-next" type="button" data-bs-target={`#${postData.id}`}
                                 data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
                             <span className="visually-hidden">Next</span>
                         </button>
-
-                        {/* THUMBNAILS */}
-                        <div className="carousel-indicators">
-                            {postData.image_urls.map((url, index) =>
-                                <button
-                                    type="button"
-                                    data-bs-target={`#${postData.id}`}
-                                    data-bs-slide-to={index}
-                                    className={`${index === 0 ? "active" : ""} thumbnail`}
-                                >
-                                    <img
-                                        src={url}
-                                        className="d-block w-100" alt="..."
-                                    />
-                                </button>
-                            )}
-                        </div>
                     </div>
                 </div>
-                <div className="col-12 col-md-6">
-                    <div className="card-body p-2 h-100 d-flex flex-column mx-2">
-                        <div className="d-flex flex-row-reverse align-items-between flex-wrap mb-2">
-                            {postData.topics.map(topic =>
-                                <TopicTag color={topic.color}>{topic.name}</TopicTag>
-                            )}
+
+                <div className="col-12 col-md-6 col-lg-6">
+                    <div className="card-body pt-0">
+                        <div className="d-flex justify-content-between align-items-start">
+                            <div className="flex-grow-1 me-3">
+                                <h3 className="card-title fw-bold">
+                                    {postData.title}
+                                </h3>
+
+                                <div className="d-flex align-items-center fw-light mb-3">
+                                    <i className="bi bi-geo-alt"></i>
+                                    <span>Poland, Warsaw</span>
+
+                                    <i className="bi bi-dot"></i>
+
+                                    <span className="badge rounded-pill text-bg-success me-5">
+                                        ECO
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="d-flex align-items-center mb-3">
+                                <img src="https://avatar.iran.liara.run/public" alt="User Avatar"
+                                     className="rounded-circle me-2"
+                                     style={{width: '50px', height: '50px'}}/>
+                                <div>
+                                    <h6 className="mb-0">John Doe</h6>
+                                </div>
+                            </div>
                         </div>
-                        <h4 className="text-center my-2">{postData.title}</h4>
-                        <p className="mb-2">{postData.description}</p>
-                        <div className="d-flex align-items-center mb-2">
-                            <Rating value={postData.rating} readOnly={true}/>
-                            <span className="ms-1">(5.0)</span>
+
+                        <div className="card-text mb-3" style={{textAlign: "justify"}}>
+                            {postData.description.length === 0 ? "No description" : postData.description}
                         </div>
-                        <div className="d-flex align-items-center mb-2">
-                            <FontAwesomeIcon icon={faLocationDot} className="me-1"/>
-                            <span>Poland, Warsaw</span>
-                        </div>
-                        <div className="d-flex align-items-center mb-4">
-                            <FontAwesomeIcon icon={faCoins} className="me-1"/>
-                            <span>{postData.price}$</span>
-                        </div>
-                        <div className="d-flex align-items-center justify-content-between my-auto mx-5">
-                            <div>
-                                <Checkbox
-                                    icon={<FontAwesomeIcon icon={emptyHeart} size={"2xl"} style={{color: "red"}}/>}
-                                    checkedIcon={<FontAwesomeIcon icon={fullHeart} size={"2xl"}
-                                                                  style={{color: "red"}}/>}
-                                    checked={postData.saved === 1}
-                                    onChange={savePost}
+
+                        <div className="d-flex align-items-center justify-content-between">
+                            <div className="d-flex align-items-center mb-3">
+                                <Rating
+                                    value={postData.rating}
+                                    icon={<FontAwesomeIcon icon={fullStar}/>}
+                                    emptyIcon={<FontAwesomeIcon icon={emptyStar}/>}
+                                    readOnly
+                                    precision={0.1}
+                                    size="small"
                                 />
+                                <span className="ms-2">({postData.rating.toFixed(1)})</span>
                             </div>
-                            <div className="me-2">
-                                <FontAwesomeIcon icon={faComment} size={"2xl"}/>
-                            </div>
-                            <FontAwesomeIcon icon={faPaperPlane} size={"2xl"}/>
                         </div>
+                        <p className="fw-bold fs-3 text-end">${postData.price}.00</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-6"></div>
+
+                <div className="col-12 col-md-6 col-lg-6">
+                    <h5 className="fw-bold mb-3">10 comments</h5>
+
+                    <div className="d-flex align-items-start mb-3 ps-3">
+                        <img src="https://via.placeholder.com/40" alt="User Avatar" className="rounded-circle me-2"
+                             style={{width: '40px', height: '40px'}}/>
+
+                        <div>
+                            <p className="mb-1">
+                                <span className="fw-bold me-2">Carla Besessen</span>
+                                <span>This is the comment text written by the user. It can span multiple lines if needed.</span>
+                            </p>
+
+
+                            <div className="d-flex align-items-center text-muted">
+                                <small className="me-3">2mo</small>
+                                <small>Reply</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="d-flex align-items-start mb-3 ps-3">
+                        <img src="https://via.placeholder.com/40" alt="User Avatar" className="rounded-circle me-2"
+                             style={{width: '40px', height: '40px'}}/>
+
+                        <div>
+                            <p className="mb-1">
+                                <span className="fw-bold me-2">Carla Besessen</span>
+                                <small className="text-muted">2mo</small>
+                            </p>
+
+
+                            <div className="d-flex align-items-center">
+                                <span>This is the comment text written by the user. It can span multiple lines if needed.</span>
+                            </div>
+
+                            <small className="text-muted">Reply</small>
+                        </div>
+                    </div>
+
+                    {/* fixme: input is nice, button is not*/}
+                    <div className="position-relative">
+                        <input
+                            type="text"
+                            className="form-control shadow-none rounded-pill py-2"
+                            placeholder="Search here..."
+                            aria-describedby="send"
+                            onChange={(e) => setCommentValue(e.target.value)}
+                            />
+                            {commentValue.trim() &&
+                                <button
+                                    className="btn btn-dark position-absolute top-50 translate-middle-y end-0 me-1 rounded-circle"
+                                    type="button"
+                                    id="send"
+                                >
+                                <FontAwesomeIcon icon={faPaperPlane} />
+                                </button>
+                            }
                     </div>
                 </div>
             </div>
