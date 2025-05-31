d95e88cdbd95ba69a602e9bc8e5c3ba96aeb58c9
kiiril <119041402+kiiril@users.noreply.github.com>
2025-05-31 17:51:48 +0200

    Redesign login and signup pages (#35)
    * Redesign login and signup pages
    
    * Imports

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,16 +1,14 @@
 import React, {useContext, useState} from 'react';
-import SubmitButton from "../components/SubmitButton";
-import axios from "axios";
 import {Link, useNavigate} from "react-router-dom";
 import {AuthContext} from "../contexts/AuthProvider";
+import PrimaryButton from "../components/PrimaryButton";
 
 const Signup = () => {
     const navigate = useNavigate();
     const {signup} = useContext(AuthContext);
 
     const [inputs, setInputs] = useState({
-        firstName: "",
-        lastName: "",
+        username: "",
         email: "",
         password: ""
     })
@@ -29,69 +27,74 @@ const Signup = () => {
     }
 
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center p-5 mx-auto vh-100">
+        <div className="d-flex align-items-center justify-content-center min-vh-100">
             {/* LOGO */}
-            <img src="/logo.png" alt="Logo"/>
+            {/*<img src="/logo.png" alt="Logo"/>*/}
 
-            <div className="w-50 bg-white rounded-4 shadow">
-                <div className="p-4">
+            <div className={"container"}>
+                <div className="row justify-content-center">
+                    <div className="col-11 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                        <div className="bg-white rounded-4 shadow-sm">
+                            <div className="p-4">
+                                <h1 className="fw-bold fs-3 mb-4 text-center">
+                                    Create account
+                                </h1>
 
-                    <h1 className="fw-bold fs-3 lh-sm mt-3 mb-4">Create account</h1>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="username" className="form-label fw-bold">Username</label>
+                                        <input className="form-control"
+                                               name="username"
+                                               id="username"
+                                               value={inputs.username}
+                                               onChange={handleChange}
+                                               type="email"
+                                               placeholder="example"
+                                        />
+                                    </div>
 
-                    <form className="mt-3" onSubmit={handleSubmit}>
-                        {/* TODO mb add fields for name and surname */}
-                        <div className="row mb-4">
-                            <div className="col">
-                                <label htmlFor="firstName" className="form-label">First Name</label>
-                                <input className="form-control"
-                                       name="firstName"
-                                       value={inputs.firstName}
-                                       onChange={handleChange}
-                                       type="text"
-                                       placeholder="John"
-                                />
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className="form-label fw-bold">Email</label>
+                                        <input className="form-control"
+                                               name="email"
+                                               value={inputs.email}
+                                               onChange={handleChange}
+                                               type="email"
+                                               placeholder="example@gmail.com"
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label htmlFor="password" className="form-label fw-bold">Password</label>
+                                        <input
+                                            className="form-control"
+                                           name="password"
+                                           value={inputs.password}
+                                           onChange={handleChange}
+                                           type="password"
+                                           placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                                           minLength={8}
+                                           required
+                                        />
+                                        <div id="passwordHelp" className="form-text">
+                                            Must have at least 8 characters.
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <PrimaryButton
+                                            text="Sign Up"
+                                            className={"w-100"}
+                                        />
+                                    </div>
+
+                                    <p className="text-center">
+                                        Already have an account?
+                                        <Link to={"/login"} className="text-decoration-none"> Log in</Link>
+                                    </p>
+                                </form>
                             </div>
-                            <div className="col">
-                                <label htmlFor="lastName" className="form-label">Last Name</label>
-                                <input className="form-control"
-                                       name="lastName"
-                                       value={inputs.lastName}
-                                       onChange={handleChange}
-                                       type="text"
-                                       placeholder="Doe"
-                                />
-                            </div>
                         </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="email" className="form-label">Your email</label>
-                            <input className="form-control"
-                                   name="email"
-                                   value={inputs.email}
-                                   onChange={handleChange}
-                                   type="email"
-                                   placeholder="example@gmail.com"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <input className="form-control"
-                                   name="password"
-                                   value={inputs.password}
-                                   onChange={handleChange}
-                                   type="password"
-                                   placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                            />
-                            <div id="passwordHelp" className="form-text">Must have at least 8 characters.</div>
-                        </div>
-
-                        <SubmitButton textColor={"white"} backgroundColor={"#ffd43b"} className={"mb-4"}>Signup</SubmitButton>
-
-                        <p className="text-center">
-                            Already have an account?
-                            <Link to={"/login"} className="text-decoration-none"> Log in</Link>
-                        </p>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
